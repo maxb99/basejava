06d6e2c275961058a79227505c560b4f12dad6d9
maxb99 <33090678+maxb99@users.noreply.github.com>
2018-09-18 11:20:10 +0300

    Update ArrayStorage.java

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,49 @@
-/**
- * Array based storage for Resumes
- */
+import java.util.Arrays;
+
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+Resume[] storage = new Resume[10000];
 
-    void clear() {
-    }
+int size = 0;
 
-    void save(Resume r) {
-    }
+void clear() {
+Arrays.fill(storage, 0, size, null);
+size =0;
+}
 
-    Resume get(String uuid) {
-        return null;
-    }
+void save(Resume resume) {
+if(size < storage.length ){
+storage[size] = resume;
+size++;
+}
+}
 
-    void delete(String uuid) {
-    }
+Resume get(String uuid) {
+if(storage[0].uuid == uuid ){
+return storage[0];
+}
+return null;
+}
 
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
-    Resume[] getAll() {
-        return new Resume[0];
-    }
+void delete(String uuid) {
+for(int i = 0; i < size; i ++) {
+if(storage[i].uuid.equals(uuid)){
+storage[i] = storage[size - 1];
+storage[size -1] = null;
+size--;
+return;
 
-    int size() {
-        return 0;
-    }
+}
+}
+
+}
+/**
+* @return array, contains only Resumes in storage (without null)
+*/
+Resume[] getAll() {
+return Arrays.copyOf(storage, size);
+}
+
+int size() {
+return 0;
+}
 }
