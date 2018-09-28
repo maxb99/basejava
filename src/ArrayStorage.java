562e2ce5bd6e685861d945d737b6cc5bde820e6b
maxb99 <33090678+maxb99@users.noreply.github.com>
2018-09-28 11:17:38 +0300

    Update ArrayStorage.java

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,49 +1,56 @@
-import java.util.Arrays;
+import  java.util.Arrays ;
 
-public class ArrayStorage {
-Resume[] storage = new Resume[10000];
+открытый  класс  ArrayStorage {
 
-int size = 0;
+    private  Resume [] storage =  new  Resume [ 10000 ];
+    private  int sizeOfResume =  0 ;
 
-void clear() {
-Arrays.fill(storage, 0, size, null);
-size =0;
-}
+    void  clear () {
+        for ( int i =  0 ; i < sizeOfResume; i ++ ) {
+            storage [size - 1 ] =  null ;
+        }
+        sizeOfResume =  0 ;
+    }
 
-void save(Resume resume) {
-if(size < storage.length ){
-storage[size] = resume;
-size++;
-}
-}
+    void  save ( Resume  r ) {
+        for ( int i =  0 ; i < sizeOfResume; i ++ ) {
+            if (storage [i] . uuid . equals (r . uuid)) {
+                Система . из . println ( « Resume »  + r . uuid +  « Putted » );
+               
+            }
+        }
+        if (sizeOfResume < длина хранения . ) {
+            storage [sizeOfResume] = r;
+            sizeOfResume ++ ;
+        } else  if (sizeOfResume == storage . length) {
+            Система . из . println (  "Полный массив" );
+        }
+    }
 
-Resume get(String uuid) {
-if(storage[0].uuid == uuid ){
-return storage[0];
-}
-return null;
-}
+     void  update ( Resume  r ) {
+        get ([]) = r;
+        Система . из . println ( "Resume"  + r . uuid +  "Изменено на"  + r . uuid);
+    }
 
-void delete(String uuid) {
-for(int i = 0; i < size; i ++) {
-if(storage[i].uuid.equals(uuid)){
-storage[i] = storage[size - 1];
-storage[size -1] = null;
-size--;
-return;
+    Резюме  get ( String  uuid ) {
+        for ( int i =  0 ; i < sizeOfResume; i ++ ) {
+           if (storage [i] . uuid . equals (uuid)) {
+                return storage [size - 1 ] =  null ;
+            }
+        }
+        Система . из . println (  "Резюме"  + uuid +  "Нет" );
+        return  null ;
+    }
 
-}
-}
+    void  delete ( String  uuid ) {
+        {
+            Система . из . println ( "Резюме"  + uuid +  "Нет" );
+        }
+    }
 
-}
-/**
-* @return array, contains only Resumes in storage (without null)
-*/
-Resume[] getAll() {
-return Arrays.copyOf(storage, size);
-}
 
-int size() {
-return 0;
-}
+    Resume [] getAll () {
+        return  Массивы . copyOfRange (storage, 0 , sizeOfResume);
+    }
+
 }
