56a44322097dbd010616163feb4bdf6c9d1e6738
maxb99 <33090678+maxb99@users.noreply.github.com>
2018-10-02 18:35:49 +0300

    Update MainTestArrayStorage.java

--- src/MainTestArrayStorage.java
+++ src/MainTestArrayStorage.java
@@ -1,36 +1,37 @@
-MainTestArrayStorage {
-    ArrayStorage  ARRAY_STORAGE  =  новый  ArrayStorage ();
+public class MainTestArrayStorage {
+    static final ArrayStorage ARRAY_STORAGE = new ArrayStorage();
 
-    public  static  void  main ( String [] args ) {
-        Резюме r1 =  новое  резюме ();
-        r1 . uuid =  " uuid1 " ;
-        Резюме r2 =  новое  резюме ();
-        r2 . uuid =  " uuid2 " ;
-        Резюме r3 =  новое  резюме ();
-        r3 . uuid =  " uuid3 " ;
+    public static void main(String[] args) {
+        Resume r1 = new Resume();
+        r1.setUuid("uuid1");
+        Resume r2 = new Resume();
+        r2.setUuid("uuid2");
+        Resume r3 = new Resume();
+        r3.setUuid("uuid3");
 
-        ARRAY_STORAGE . сохранить (r1);
-        ARRAY_STORAGE . сохранить (r2);
-        ARRAY_STORAGE . сохранить (r3);
+        ARRAY_STORAGE.save(r1);
+        ARRAY_STORAGE.save(r2);
+        ARRAY_STORAGE.save(r3);
 
-        Система . из . println ( « Получить r1: »  +  ARRAY_STORAGE . get (r1 . uuid));
-        Система . из . println ( " Размер: "  +  ARRAY_STORAGE . size ());
+        System.out.println("Get r1: " + ARRAY_STORAGE.get(r1.getUuid()));
+        ARRAY_STORAGE.update(r2);
+        System.out.println("Size: " + ARRAY_STORAGE.size());
 
-        Система . из . println ( « Получить манекен: »  +  ARRAY_STORAGE . get ( « фиктивный » ));
+        System.out.println("Get dummy: " + ARRAY_STORAGE.get("dummy"));
 
-        Printall ();
-        ARRAY_STORAGE . delete (r1 . uuid);
-        Printall ();
-        ARRAY_STORAGE . Чисто();
-        Printall ();
+        printAll();
+        ARRAY_STORAGE.delete(r1.getUuid());
+        printAll();
+        ARRAY_STORAGE.clear();
+        printAll();
 
-        Система . из . println ( " Размер: "  +  ARRAY_STORAGE . size ());
+        System.out.println("Size: " + ARRAY_STORAGE.size());
     }
 
-    static  void  printAll () {
-        Система . из . println ( " \ n Получить все " );
-        for ( Resume r :  ARRAY_STORAGE . getAll ()) {
-            Система . из . Println (г);
+    static void printAll() {
+        System.out.println("\nGet All");
+        for (Resume r : ARRAY_STORAGE.getAll()) {
+            System.out.println(r);
         }
     }
 }
