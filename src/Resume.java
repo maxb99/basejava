9e5a8f60250854889e377a835d724dd420490d77
maxb99 <33090678+maxb99@users.noreply.github.com>
2018-10-18 10:02:38 +0300

    Update Resume.java

--- src/Resume.java
+++ src/Resume.java
@@ -3,6 +3,14 @@
  */
 public class Resume {
 
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
     // Unique identifier
     String uuid;
 
@@ -10,4 +18,5 @@ public class Resume {
     public String toString() {
         return uuid;
     }
+
 }
