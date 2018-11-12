c71ab5218ef1a9bf2a4f1bdc30605043a0c9aab2
maxb99 <33090678+maxb99@users.noreply.github.com>
2018-11-12 11:13:23 +0300

    Update ArrayStorage.java

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,80 +1,34 @@
-import java.util.Arrays;
+package com.urise.webapp.storage;
 
-public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+        import com.urise.webapp.model.Resume;
 
-    int size = 0;
+        import java.util.Arrays;
 
-    void clear() {
-        Arrays.fill(storage, 0, size, null);
-        size = 0;
+/**
+ * Array based storage for Resumes
+ */
+public class ArrayStorage extends AbstractArrayStorage {
+
+    @Override
+    protected void doDelete(int index) {
+        storage[index] = storage[size - 1];
     }
 
-    void save(Resume resume) {
-        if (getIndex(resume.getUuid()) != -1) {
-            System.out.println("This summary is already stored");
-        } else if (size == storage.length) {
-            System.out.println("All places are occupied");
-        } else {
-            storage[size] = resume;
-            size++;
-        }
+    @Override
+    protected void doSave(Resume r, int index) {
+        storage[size] = r;
     }
 
-    void update(Resume r) {
-        int i = getIndex(r.getUuid());
-        if (i == -1) {
-            System.out.println("There is no such resume.");
-        } else {
-            storage[i] = r;
-        }
-    }
 
-    Resume get(String uuid) {
-        int i = getIndex(uuid);
-        if (i == -1) {
-            System.out.println("There is no such resume.");
-            return null;
-        } else {
-            return storage[i];
-        }
-    }
-
-    void delete(String uuid) {
-        int i = getIndex(uuid);
-        if (i == -1) {
-            System.out.println("There is no such resume.");
-        } else {
-            storage[i] = storage[size - 1];
-            storage[size - 1] = null;
-            size--;
-        }
-    }
-
-    {
-        /**
-         * @return array, contains only Resumes in storage (without null)
-         */
-    }
-
-    Resume[] getAll() {
-        return Arrays.copyOf(storage, size);
-    }
-
-    int size() {
-        return size;
-    }
-
-    private int getIndex(String uuid) {
+    protected int getIndex(String uuid) {
         for (int i = 0; i < size; i++) {
-            if (storage[i].getUuid().equals(uuid)) {
+            if (uuid.equals(storage[i].getUuid())) {
                 return i;
             }
         }
         return -1;
     }
 }
-
 
 
 
